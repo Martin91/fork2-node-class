0b8639fb13612d1c27a763636e56c9b5eeccc729
Martin <hongzeqin@gmail.com>
2014-03-28 00:25:59 +0800

    implement instance methods

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 module.exports = Class;
 
 function Class(options){
-  return (options.initialize ? options.initialize : function(){});
+  var __TargetClass__ = options.initialize ? options.initialize : function(){};
+  for(key in options) {
+    if(key != 'initialize') {
+      __TargetClass__.prototype[key] = options[key];
+    }
+  }
+  return __TargetClass__;
 }
