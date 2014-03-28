05d1850e78583b5540d514a28a59a6e27b56225b
Martin <hongzeqin@gmail.com>
2014-03-28 13:46:50 +0800

    fix an error

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ module.exports = Class;
 function Class(options, parentClass){
   var __TargetClass__ = options.initialize ? options.initialize : function(){};
 
-  for(key in options) {
-    if(key != 'initialize') {
-      __TargetClass__.prototype[key] = options[key];
-    }
-  }
-
   if(parentClass) {
     __TargetClass__.prototype = new parentClass(arguments);
     __TargetClass__.prototype.constructor = __TargetClass__;
     __TargetClass__.__super__ = parentClass;
   } else {
     __TargetClass__.__super__ = Object;
+  }
+
+  for(key in options) {
+    if(key != 'initialize') {
+      __TargetClass__.prototype[key] = options[key];
+    }
   }
 
   return __TargetClass__;
