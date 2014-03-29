8a7e91b70ab596515ac6b4b9c61eb3cfe7dbeb2a
Martin <hongzeqin@gmail.com>
2014-03-29 14:38:42 +0800

    append some comments

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 module.exports = Class;
 
 function Class(prop, parentClass){
+  // Initialize the constructor
   var klass = prop.initialize ? prop.initialize : function(){};
 
   if(parentClass) {
+    // Inheritance from the parent class
     klass.prototype = new parentClass();
     klass.prototype.constructor = klass;
+
+    // Use __super__ to notate the parent class
     klass.__super__ = parentClass;
 
+    // Implement the super method
     var currentClass = klass;
     klass.prototype.super = function(_method){
       currentClass = currentClass.__super__;
@@ -22,6 +27,7 @@ function Class(prop, parentClass){
   if(parentClass) {
     var _super = klass.__super__.prototype;
   }
+
   for(method in prop) {
     if(method != 'initialize') {
       // Determine if the current method is existed in the super class
@@ -29,11 +35,12 @@ function Class(prop, parentClass){
         (parentClass && (typeof prop[method]) == 'function' && _super.hasOwnProperty(method) && (typeof _super[method]) == 'function') ?
           (function(method, func) {
             return function(){
+              // Replace the this._super temporarily
               var tmp = this._super;
               this._super = _super[method];
               var res = func.apply(this, arguments);
-
               this._super = tmp;
+
               return res;
             };
           })(method, prop[method]) : prop[method];
